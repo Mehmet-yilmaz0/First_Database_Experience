a3fc3c60e618c0d31e01156f3300a849ce63a51c
Mehmet-Yılmaz <Mhmt_ylmz_@outlook.com>
2024-12-11 14:48:26 +0300

    Add files via upload
    final form is uploaded.

---
+++ TableView/view/Front.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace SqlDers
+{
+    partial class Front
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+
+            this.SuspendLayout();
+            // 
+            // Connect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Connect";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Connect_Load);
+            this.ResumeLayout(false);
+
+        }
+
+
+        #endregion
+    }
+}
+
